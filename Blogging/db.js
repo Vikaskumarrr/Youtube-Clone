c71c53a67c297a488b9ae0fc3a4c9bab893eaadc
Vikas Bhandari <143922604+Vikaskumarrr@users.noreply.github.com>
2025-02-11 14:21:41 +0530

    Delete Blogging directory

--- Blogging/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const clc = require("cli-color");
-
-mongoose
-.connect(process.env.MONGO_URI)
-.then(()=>console.log(clc.yellowBright("MongoDB connect sucessfully")))
-.catch((error)=>console.log(clc.redBright(error)));
